d21f243ce6cd65c8b4d42679be27355261b1b339
Prabesh Neupane <rodrijack5@gmail.com>
2020-11-19 17:10:43 +0545

    with registry

--- assets/js/mystyle.js
+++ assets/js/mystyle.js
@@ -2,4 +2,6 @@ const date = new Date().getFullYear()
 
 document.getElementById('dateNow').innerHTML = `Copyright &copy; ${date}`
 
-
+function searchAppearFunction() {
+    document.getElementById('searchAppear').classList.remove("d-none");
+}
