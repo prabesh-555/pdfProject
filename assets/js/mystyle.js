89026e99795d8b2d4ea74e23e43c872c53db3862
Prabesh Neupane <rodrijack5@gmail.com>
2020-11-29 16:38:27 +0545

    notes

--- assets/js/mystyle.js
+++ assets/js/mystyle.js
@@ -2,6 +2,10 @@ const date = new Date().getFullYear()
 
 document.getElementById('dateNow').innerHTML = `Copyright &copy; ${date}`
 
-function searchAppearFunction() {
-    document.getElementById('searchAppear').classList.remove("d-none");
+
+function hideSearchBox() {
+    document.getElementById("search").classList.remove("d-none")   
+}
+function HideSearchBox() {
+    document.getElementById("search").classList.add("d-none")   
 }
